23d2ed72321cd41b37354f4f6f401fcf468e4b81
6yy66yy <1390291793@qq.com>
2022-04-29 16:48:04 +0800

    弃用

--- run.py
+++ run.py
@@ -1,32 +1,33 @@
-###############
-# @Author: 6yy66yy
-# @Date: 2022-03-11 14:13:00
-# @LastEditors: 6yy66yy
-# @LastEditTime: 2022-03-24 15:49:33
-# @FilePath: \legod-auto-pause\run.py
-# @Description: 主程序，控制托盘和暂停程序，不可独立运行
-###############
+# ###############
+# # @Author: 6yy66yy
+# # @Date: 2022-03-11 14:13:00
+# # @LastEditors: 6yy66yy
+# # @LastEditTime: 2022-04-29 16:46:21
+# # @FilePath: \legod-auto-pause\run.py
+# # @Description: 已弃用
+# ###############
 
-import legod
-import TrayIcon
-import multiprocessing
-import win32gui 
-from multiprocessing import Pipe
-def gui(conn2):
-    # print(os.getpid())
-    t = TrayIcon.TrayIcon()
-    win32gui.PumpMessages()
-    conn2.send('A') #发送标志将后台程序关闭
-def houtai(conn1):
-    # print(os.getpid())
-    legod.detection(conn1)
-if __name__=='__main__':
-    multiprocessing.freeze_support()
-    conn1, conn2 = Pipe(duplex=True)  # 开启双向管道，管道两端都能存取数据。默认开启
-    pool = multiprocessing.Pool(processes=2)
-    pool.apply_async(houtai,(conn1,))
-    pool.apply_async(gui,(conn2,))
+# import legod
+# import TrayIcon
+# import multiprocessing
+# import win32gui 
+# from multiprocessing import Pipe
+# def gui(conn2,gclass):
+#     # print(os.getpid())
+#     t = TrayIcon.TrayIcon(gclass)
+#     win32gui.PumpMessages()
+#     conn2.send('close') #发送标志将后台程序关闭
+# def houtai(conn1,gclass):
+#     # print(os.getpid())
+#     gclass.detection(conn1)
+# if __name__=='__main__':
+#     multiprocessing.freeze_support()
+#     conn1, conn2 = Pipe(duplex=True)  # 开启双向管道，管道两端都能存取数据。默认开启
+#     gclass=legod.legod(True)
+#     pool = multiprocessing.Pool(processes=2)
+#     pool.apply_async(houtai,(conn1,gclass,))
+#     pool.apply_async(gui,(conn2,gclass,))
     
-    pool.close()
-    pool.join()
-    print ("所有进程结束")
+#     pool.close()
+#     pool.join()
+#     print ("所有进程结束")
